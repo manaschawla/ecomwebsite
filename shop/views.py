c9b6303b7bb6fb264440c2f8a2e2febdce8020c7
manaschawla <manaschawla324@gmail.com>
2025-05-03 19:44:38 +0530

    intialized ecom website and created first page

---
+++ shop/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse
+
+# Create your views here.
+def index(request):
+    return render(request, 'shop/index.html')
+
+def about(request):
+    return render(request, 'shop/index.html')
+
+def contact(request):
+    return render(request, 'shop/index.html')
+
+def tracker(request):
+    return render(request, 'shop/index.html')
+
+def search(request):
+    return render(request, 'shop/index.html')
+
+def prodview(request):
+    return render(request, 'shop/index.html')
+
+def checkout(request):
+    return HttpResponse("hello")
