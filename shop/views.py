ae26e277e5ce2861814b7526423301b4276fb6d8
manaschawla <manaschawla324@gmail.com>
2025-05-10 14:01:10 +0530

    enhance the homepage and created different sections

--- shop/views.py
+++ shop/views.py
@@ -7,10 +7,19 @@ def index(request):
     products = Product.objects.all()
     print(products)
     print("Total products:", len(products))
-    n = len(products)
-    nSlides = nSlides = ceil(n / 4)
-    params = {'no_of_slides': nSlides , 'range': range(nSlides) ,'product' :  products}
-    return render(request, 'shop/index.html', params)
+    #params = {'no_of_slides': nSlides , 'range': range(nSlides) ,'product' :  products}
+    #allprods = [[products, range(1, nSlides), nSlides],
+        #       [products, range(1, nSlides), nSlides]]
+    allprods = []
+    catprod = Product.objects.values('category', 'id')
+    cats = {item['category'] for item in catprod}
+    for cat in cats:
+        n = len(products)
+        nSlides = nSlides = ceil(n / 4)
+        prod = Product.objects.filter(category = cat)
+        allprods.append([prod, range(1, nSlides), nSlides])
+    params = {'allprods': allprods}
+    return render(request, 'shop/index.html', params )
 
 
 def about(request):
